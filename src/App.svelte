aa94a82d08c5adcee8de3396f930b0069e391594
Camille Dbschr <c.debusscher@gmail.com>
2023-03-24 11:48:14 +0100

    fix: delete Component Tailwind, add by postcss

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-  import TailwindCss from './lib/TailwindCSS.svelte';
-  import Nav from './components/Nav.svelte';
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
-  <TailwindCss />
-  <Nav />
   <div class="flex">
     <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
       <img src={viteLogo} class="logo" alt="Vite Logo" />
@@ -24,12 +20,11 @@
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>,
+    the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  plugins: [],
+  darkMode: 'class',
   theme: {
-    extend: {}
-  },
-  plugins: []
+    extend: {
+      colors: {
+        // violet
+        primary: {"50":"#f5f3ff","100":"#ede9fe","200":"#ddd6fe","300":"#c4b5fd","400":"#a78bfa","500":"#8b5cf6","600":"#7c3aed","700":"#6d28d9","800":"#5b21b6","900":"#4c1d95"}
+      }
+    }
+  }
 };
